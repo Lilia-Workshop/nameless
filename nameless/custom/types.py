2f99fe951538beed285f93b17f39a98c27f6e5b9
Tien Dat Pham <dat20036@gmail.com>
2025-01-30 12:28:25 +0700

    PEP695 is not ready for now.

--- nameless/custom/types.py
+++ nameless/custom/types.py
@@ -1,10 +1,6 @@
-from typing import TypeAlias
-
 import discord
 
 __all__ = ["NamelessTextable"]
 
-NamelessTextable: TypeAlias = (
-    discord.TextChannel | discord.Thread | discord.VoiceChannel
-)
+NamelessTextable = discord.TextChannel | discord.Thread | discord.VoiceChannel
 """Channels that retrieving & sending data without quirks."""
